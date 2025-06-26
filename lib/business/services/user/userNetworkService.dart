48c64f927daa337dce2ec361d75a4e96388e52df
Mgreat01 <ephraimmonga5@gmail.com>
2025-06-26 17:36:26 +0200

    creation et implementation des methodes pour creer un compte , se deconnecter , recuperer les centres d interet et pour verifier l otp

--- lib/business/services/user/userNetworkService.dart
+++ lib/business/services/user/userNetworkService.dart
@@ -1,7 +1,13 @@
 import '../../models/user/authentication.dart';
+import '../../models/user/interet.dart';
 import '../../models/user/user.dart';
+import '../../models/user/registerRequest.dart';
 
 abstract class UserNetworkService {
   Future<User> seConnecter(Authentication authentication);
   Future<User> recupererInfoUtilisateur();
+  Future<User> creerCompte(RegisterRequest registerRequest);
+  Future<User> seDeconnecter();
+  Future<List<Interet>> getInterets();
+  Future<bool> verifierOtp({required String email, required String otp});
 }
