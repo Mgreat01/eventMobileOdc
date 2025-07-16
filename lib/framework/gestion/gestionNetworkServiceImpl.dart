deb450185c1a1d211b4960ca1e313392ba65d1a9
Mgreat01 <ephraimmonga5@gmail.com>
2025-07-16 15:14:59 +0200

    creation de deux nouvelles methodes pour la recuperation de evenements dans le back et implementation de l une d elles
    _ creation des methodes ( recuperEventById et recupererEvents ) dans la class abstraite  GestionNetworkService
    
    - implementation de la methode recupererEvents dans GestionNetworkServiceImpl

--- lib/framework/gestion/gestionNetworkServiceImpl.dart
+++ lib/framework/gestion/gestionNetworkServiceImpl.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:odc_mobile_template/business/models/article/event.dart';
+
 import '../../business/models/article/article.dart';
 import '../../business/services/gestion/gestionNetworkService.dart';
 import '../../utils/http/HttpUtils.dart';
@@ -25,4 +27,31 @@ class GestionNetworkServiceImpl implements GestionNetworkService {
         listData.map<Article>((e) => Article.fromJson(e)).toList();
     return listArticles;
   }
-}
+
+  @override
+  Future<Event> recuperEventById(int id) {
+    // TODO: implement recuperEventById
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Event>> recupererEvents() async {
+    var url = '$baseUrl/events';
+    var response = await httpUtils.getData(url);
+
+    // Si response est un JSON encodé (String), décode-le d’abord
+    final jsonResponse = response is String ? jsonDecode(response) : response;
+
+    if (jsonResponse is Map<String, dynamic> && jsonResponse.containsKey('data')) {
+      List<dynamic> eventData = jsonResponse['data'];
+      print("les événements venant du backend : $eventData");
+      return eventData.map((event) => Event.fromJson(event)).toList();
+    } else {
+      throw Exception("Format de réponse inattendu lors de la récupération des événements");
+    }
+  }
+
+  }
+
+
+
